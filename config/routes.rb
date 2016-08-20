9146b0de6d706ec6af76a4fec360f57263adacaa
Scott McAllister <scottm@nextopia.com>
2016-08-20 13:25:08 -0400

    make site home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
-  resources :polls
-  resources :groups
-  root to: 'polls#index'
+
+  authenticated :user do
+    root 'polls#index', as: :authenticated_root
+  end
+
+  root "home#index"
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#auth_fail'
   get '/sign_out', to: 'sessions#destroy', as: :sign_out
   resources :votes, only: [:create]
   resources :users, only: [:show]
+  resources :polls
+  resources :groups
+
 end
