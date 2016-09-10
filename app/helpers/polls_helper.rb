4061089d44e6086990d0a33a0d8e72d26818de2f
Scott McAllister <scottm@nextopia.com>
2016-09-09 21:22:33 -0400

    finish poll detail page

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,9 +1,12 @@
+# TODO: change to Bootstrap 4 markup
 module PollsHelper
   def visualize_votes_for(option)
-    content_tag :div, class: 'progress' do
-      content_tag :div, class: 'progress-bar',
-                  style: "width: #{option.poll.normalized_votes_for(option)}%" do
-        "#{option.votes.count}"
+    content_tag :progress, value: "#{option.poll.normalized_votes_for(option)}", max: "100" do
+      content_tag :div, class: 'progress' do
+        content_tag :div, class: 'progress-bar',
+                    style: "width: #{option.poll.normalized_votes_for(option)}%" do
+          "#{option.votes.count}"
+        end
       end
     end
   end
